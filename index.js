2fd545e6763c3bde189b7d623220627534b8cec3
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-23 22:16:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function getFirstSelector(selector){
+return  document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+function increaseRankBy(n){
+  var ranking = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i< ranking.length; i++){
+    ranking[i].innerHTML = parseInt(ranking[i].innerHTML)+ n
+  }
+}
+function deepestChild(){
+ var deep = document.getElementById('grand-node').querySelectorAll('div')
+ return deep[deep.length-1]
+}
